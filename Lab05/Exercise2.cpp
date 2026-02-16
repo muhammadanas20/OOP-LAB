196b0bb9ce01cdd65bbd1f84e8403981c62ea2d6
Muhammad Anas <muhammadanasashfaq2006@gmail.com>
2026-02-16 09:05:11 +0500

    Create Exercise2.cpp

---
+++ Lab05/Exercise2.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+class BankAccount {
+    private:
+    static double interestRate;
+    public:
+   static void showinterestrate(){
+        cout << "interest rate:" << interestRate << endl;
+    }
+};
+
+double BankAccount::interestRate = 15.5;
+
+int main() {
+     BankAccount a1;
+     BankAccount::showinterestrate();
+     return 0;
+}
