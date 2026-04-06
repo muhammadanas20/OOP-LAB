4d751781d09ede9032ea5ff5488ee493883a8e97
Muhammad Anas <muhammadanasashfaq2006@gmail.com>
2026-04-06 09:43:26 +0500

    Add Student class with marks and average calculation
    Implemented a Student class that allows setting marks and calculating the average.

--- lab08/Tasks/task1.cpp
+++ lab08/Tasks/task1.cpp
@@ -1,1 +1,40 @@
 
+#include <iostream>
+using namespace std;
+class Student{
+	private:
+		int marks[3];
+	public:
+		friend float avg(Student s);
+		void setMarks(){
+			int i=0;
+			while(i<3){
+				int temp;
+				cout << "Enter a marks for subject " << i+1 << " :";
+				cin >> temp;
+				if(temp >= 0 && temp <= 100){
+				marks[i++] = temp;
+			   }else{
+			   	cout << "Invalid input\n";
+			   }
+			  	
+			}
+			 cout << "Max subject reachead\n";
+			 return;
+			  
+		}
+		
+};
+float avg(Student s){
+	int i=0;
+	int sum = 0;
+	for(i=0;i<3;i++){
+		sum += s.marks[i];
+	}
+	return sum/3;
+}
+int main(){
+	Student s;
+	s.setMarks();
+	cout << "Avg of student :" << avg(s) << endl;
+}
